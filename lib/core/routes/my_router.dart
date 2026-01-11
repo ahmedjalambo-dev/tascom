719f8d026a9fb8dbd5a0fd36f648a0ef922c37e9
LayanMDamrah <2layanmajed@gmail.com>
2026-01-11 18:53:57 +0200

    onboarding screens

--- lib/core/routes/my_router.dart
+++ lib/core/routes/my_router.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tascom/core/routes/my_routes.dart';
+import 'package:tascom/features/onboarding/onboarding._screens.dart';
 
 class MyRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case MyRoutes.onboarding:
+        
         return MaterialPageRoute(
-          builder: (context) =>
-              const Scaffold(body: Center(child: Text('Onboarding Screen'))),
+          builder: (context) => const OnBoardingScreen(),
         );
 
       default:
