b39578ef8879515a1c93fedccee4a7ade7373892
ahmedjalambo-dev <ahmedjalambo.dev@gmail.com>
2026-01-28 11:04:17 +0200

    refactor: remove unused route and theme imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tascom/features/profile/ui/screen/profile_screen.dart';
-import 'package:tascom/core/routes/my_routes.dart';
-import 'package:tascom/core/routes/my_router.dart';
-import 'package:tascom/core/themes/my_theme.dart';
 
 void main() {
   runApp(const MyApp());
